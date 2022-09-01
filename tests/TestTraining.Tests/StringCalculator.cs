8a84b96325f3faaa3929fc9e412d740a3a06abe9
Yassin <yguelmament@telkea.com>
2022-09-01 17:43:08 +0200

    RAB

--- tests/TestTraining.Tests/StringCalculator.cs
+++ tests/TestTraining.Tests/StringCalculator.cs
@@ -4,5 +4,15 @@ namespace TestTraining.Tests;
 
 public class StringCalculator
 {
-    public int Add(string numbers) => throw new NotImplementedException();
+    public int Add(string numbers)
+    {
+        var result = 0;
+        if (string.Equals(numbers, string.Empty))
+            return result;
+        foreach (var theNumber in numbers.Split(','))
+        {
+            result += int.Parse(theNumber);
+        }
+        return result;
+    }
 }
--- tests/TestTraining.Tests/StringCalculatorTest.cs
+++ tests/TestTraining.Tests/StringCalculatorTest.cs
@@ -1,4 +1,5 @@
 using System;
+using FluentAssertions;
 using Xunit;
 
 namespace TestTraining.Tests;
@@ -13,8 +14,17 @@ public class StringCalculatorTest
     }
 
     [Fact]
-    public void Add_ShouldReturnZero_GivenNumbersIsEmptyString()
-    {
-        throw new NotImplementedException();
-    }
+    public void Add_ShouldReturnZero_GivenNumbersIsEmptyString() => this.calculator.Add(string.Empty).Should().Be(0);
+
+    [Fact]
+    public void Add_ShouldReturnOne_GivenNumbersIsOne() => this.calculator.Add("1").Should().Be(1);
+
+    [Fact]
+    public void Add_ShouldReturnTwo_GivenNumbersIsTwo() => this.calculator.Add("2").Should().Be(2);
+
+    [Fact]
+    public void Add_ShouldReturnThree_GivenNumbersIsOneAndTwo() => this.calculator.Add("1,2").Should().Be(3);
+
+    [Fact]
+    public void Add_ShouldReturnSix_GivenNumbersIsOneAndTwoAndThree() => this.calculator.Add("1,2,3").Should().Be(6);
 }
